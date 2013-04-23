24e76c3cdac5510156102bc7e583ec57d3deca31
Adam Litke <agl@linux.vnet.ibm.com>
2013-04-23 11:31:10 -0500

    ui: Refresh UI at a 5 second interval
    Keep the UI view current by refreshing the data every 5 seconds.
    
    Signed-off-by: Adam Litke <agl@linux.vnet.ibm.com>
    Message-id: 1366734670-20097-1-git-send-email-agl@linux.vnet.ibm.com

--- js/main.js
+++ js/main.js
@@ -37,14 +37,19 @@ function genPeer(name)
 
 function load_vms(data)
 {
+    var sel_vms;
     var html = "";
     var i;
 
+    sel_vms = getSelectedVMs();
+
+    $("#vms").empty();
     for (i = 0; i < data.length; i++) {
         html += genTile(data[i].name, data[i].screenshot,
                         data[i].state != 'running', false);
     }
     $("#vms").append(html);
+    selectVMs(sel_vms);
 
     $(".icon").click(function() {
         if (this.className.indexOf("selected") == -1) {
@@ -60,23 +65,48 @@ function load_templates(data)
     var html = "";
     var i;
 
+    $("#templates").empty();
     for (i = 0; i < data.length; i++) {
         html += genTile(data[i].name, data[i].icon, false, true);
     }
-    $("#templates").prepend(html);
+    html += genTile("Create New Templates from ISOS", "images/image-missing.svg", false, true);
+    html += genTile("Create New Template from Guests", "images/image-missing.svg", false, true);
+    $("#templates").append(html);
 }
 
-function load(data)
+function load_peers(data)
 {
     var html = "";
     var i;
 
-    for (i = 0; i < data.peers.length; i++) {
-	html += genPeer(data.peers[i]);
+    $("ul.projects").empty();
+    for (i = 0; i < data.length; i++) {
+        html += genPeer(data[i]);
     }
     $("ul.projects").append(html);
+}
+
+function load(data)
+{
+    console.log("load");
+    $.ajax({
+	url: "/vms",
+	dataType: "json"
+    }).done(load_vms);
+
+    $.ajax({
+	url: "/templates",
+	dataType: "json"
+    }).done(load_templates);
+
+    $.ajax({
+    url: "/peers",
+    dataType: "json",
+    }).done(load_peers);
 
     $("div.icon").draggable({ revert: true });
+
+    window.setTimeout("load();", 5000);
 }
 
 function getSelectedVMs()
@@ -92,32 +122,27 @@ function getSelectedVMs()
     return names
 }
 
+function selectVMs(names)
+{
+    var icons = $("#vms .icon");
+    var i;
+
+    for (i = 0; i < icons.length; i++) {
+        var id = icons[i].id;
+        if (names.indexOf(id) >= 0) {
+            icons[i].className = icons[i].className + " selected";
+        }
+    }
+}
+
 function start()
 {
     var html = "";
 
-    html += genTile("Create New Templates from ISOS", "images/image-missing.svg", false, true);
-    html += genTile("Create New Template from Guests", "images/image-missing.svg", false, true);
-    $("#templates").append(html);
-
     html = genTile("Create Guest", "images/image-missing.svg", false, true);
     $("#custom").append(html);
 
-    $.ajax({
-	url: "/vms",
-	dataType: "json"
-    }).done(load_vms);
-
-    $.ajax({
-	url: "/templates",
-	dataType: "json"
-    }).done(load_templates);
-
-
-    $.ajax({
-	url: "rest/guests",
-	dataType: "json"
-    }).done(load);
+    load();
 
     $(".icon-play").click(function() {
         vms = getSelectedVMs();
