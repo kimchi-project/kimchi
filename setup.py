8a36c45651ffdb06a7452f006022d28455641eb8
ShaoHe Feng <shaohef@linux.vnet.ibm.com>
2013-05-21 16:51:14 +0800

    generate mo files during build time
    mo files are generated during build time.
    $python setup.py build
    
    Also add another command 'make_mo' to generated mo files.
    This command does nothing except generates mo files.
    $ python setup.py make_mo
    
    Signed-off-by: ShaoHe Feng <shaohef@linux.vnet.ibm.com>
    Reviewed-by: Aline Manera <alinefm@br.ibm.com>

--- setup.py
+++ setup.py
@@ -10,14 +10,61 @@
 # All Rights Reserved.
 #
 
+from distutils.command.build import build
+from distutils.cmd import Command
 from distutils.core import setup
-from glob import glob
+from glob import glob, iglob
+import polib
+import os
+
+
+PROJECT = 'burnet'
+
+
+def make_mo():
+    print "compile mo files"
+    for path in iglob("i18n/po/*"):
+        pofile = os.path.join(path, 'LC_MESSAGES', PROJECT + '.po')
+        lang = path.rsplit('/', 1)[1]
+        mopath = os.path.join("i18n/mo", lang, 'LC_MESSAGES')
+        mofile = os.path.join(mopath, PROJECT + '.mo')
+        if (os.path.isfile(pofile) and
+            (not os.path.isfile(mofile) or
+             os.path.getmtime(mofile) < os.path.getmtime(pofile))):
+            po = polib.pofile(pofile)
+            if not os.path.isdir(mopath):
+                os.makedirs(mopath)
+            po.save_as_mofile(mofile)
+            print "compile: %s" % mofile
+
+
+class cmd_make_mo(Command):
+    description = "compile po files to mo files"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        make_mo()
+
+
+class burnet_build(build):
+    def run(self):
+        make_mo()
+        build.run(self)
+
 
 setup(name='burnet',
       version='0.1',
       package_dir={'': 'src'},
       packages=['burnet'],
       scripts=['bin/burnetd'],
+      cmdclass={'make_mo': cmd_make_mo,
+                'build': burnet_build},
       data_files=[('share/burnet/js', glob('js/*.js')),
                   ('share/burnet/css', glob('css/*.css')),
                   ('share/burnet/css/fonts', glob('css/fonts/*')),
@@ -28,4 +75,5 @@ setup(name='burnet',
                    glob('static/include/web-socket-js/*')),
                   ('share/burnet/data/screenshots', []),
                   ('share/burnet/images', glob('images/*')),
-                  ('share/burnet/templates', glob('templates/*.tmpl'))])
+                  ('share/burnet/templates', glob('templates/*.tmpl')),
+                  ('share/burnet/i18n', glob('i18n/*'))])
