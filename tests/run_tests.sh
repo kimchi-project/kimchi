69669a1df26d0639e6d5d15005e7748f4c086049
Royce Lv <lvroyce@linux.vnet.ibm.com>
2013-07-10 18:12:48 +0800

    test: change testcase to allow running single test
    v2>v3, rebase to latest.
    
    run_test can be used from the command line to run tests from modules,
    classes or even individual test methods. When with no parameter,
    run_tests.sh will run all testcases under /tests.
    
    run_tests.sh test_module1 test_module2
    run_tests.sh test_module.TestClass
    run_tests.sh test_module.TestClass.test_method
    run_tests.sh
    
    Signed-off-by: Royce Lv <lvroyce@linux.vnet.ibm.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -21,5 +21,9 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-PYTHONPATH=../src \
-  python -m unittest discover -p 'test_*.py'
+if [ $# -ne 0 ]
+  then
+      PYTHONPATH=../src python -m unittest $@
+  else
+      PYTHONPATH=../src  python -m unittest discover -p 'test_*.py'
+fi
