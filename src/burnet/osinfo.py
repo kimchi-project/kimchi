8796558bdeab4f3a541802bd4410a15b1a6731b4
Royce Lv <lvroyce@linux.vnet.ibm.com>
2013-07-17 14:47:04 +0800

    template: add rhel to osinfo
    rhel iso was probed to be unknown distro and this is misleading,
    add rhel distro information to osinfo
    
    Signed-off-by: Royce Lv <lvroyce@linux.vnet.ibm.com>

--- src/burnet/osinfo.py
+++ src/burnet/osinfo.py
@@ -102,6 +102,25 @@ osinfo = [
         'disk_bus': 'ide', 'nic_model': 'e1000',
         'cdrom_bus': 'ide', 'cdrom_index': 2,
     }),
+    ('rhel', {
+        'version': lambda d,v: bool(d == 'rhel' and
+            v.startswith('6.')),
+        'icon': 'images/icon-vm.svg',
+        'cpus': 1, 'cpu_cores': 1, 'cpu_threads': 1,
+        'memory': 1024,
+        'disks': [{'index': 0, 'size': 10}],
+        'disk_bus': 'virtio', 'nic_model': 'virtio',
+        'cdrom_bus': 'ide', 'cdrom_index': 2,
+    }),
+    ('rhel-old', {
+        'version': lambda d,v: bool(d == 'rhel'),
+        'icon': 'images/icon-vm.png',
+        'cpus': 1, 'cpu_cores': 1, 'cpu_threads': 1,
+        'memory': 1024,
+        'disks': [{'index': 0, 'size': 10}],
+        'disk_bus': 'ide', 'nic_model': 'e1000',
+        'cdrom_bus': 'ide', 'cdrom_index': 2,
+    }),
     ('unknown', {
         'version': lambda d,v: True,
         'icon': 'images/icon-vm.svg',
