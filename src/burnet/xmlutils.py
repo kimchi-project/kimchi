cbd5afae14b47b35f97689abe6c389275b75b687
ShaoHe Feng <shaohef@linux.vnet.ibm.com>
2013-07-22 17:04:05 +0800

    fix bug: xmlutils get node context by context attribute
    doc: http://www.xmlsoft.org/python.html
    content : returns the content of the node, it is based on
              xmlNodeGetContent() and hence is recursive.
    parent, children, last, next, prev, doc, properties:
            pointing to the associated element in the tree,
            those may return None in case no such link exists.
    
    ref: http://www.xml.com/lpt/a/1211
    for the ref, we should use context instead of str
    
    bug: https://github.com/kimchi-project/kimchi/issues/69
    
    Signed-off-by: ShaoHe Feng <shaohef@linux.vnet.ibm.com>
    Signed-off-by: Bing Bu Cao <mars@linux.vnet.ibm.com>

--- src/burnet/xmlutils.py
+++ src/burnet/xmlutils.py
@@ -25,7 +25,7 @@ import libxml2
 def xpath_get_text(xml, expr):
     doc = libxml2.parseDoc(xml)
     res = doc.xpathEval(expr)
-    ret = [ str(x.children) for x in res ]
+    ret = [None if x.children == None else x.children.content for x in res]
 
     doc.freeDoc()
     return ret
