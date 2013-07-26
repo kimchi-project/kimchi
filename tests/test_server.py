30bc539a4988917ea701f6b2aa68bdfc0cef6feb
Adam Litke <agl@linux.vnet.ibm.com>
2013-07-26 15:01:21 -0400

    tests: Fix test_server_start
    Commit 7e963feb5374f2cec013df6ac23305a146a42dfb changed the data that
    can be expected from '/' in the REST API.  This is intended behavior but
    test_server_start() needs to be updated with the new behavior.
    
    Everyone please do your reviewers a favor and run the test suite on all
    patches before submitting them.
    
    Signed-off-by: Adam Litke <agl@linux.vnet.ibm.com>
    Message-id: 1374865281-22101-1-git-send-email-agl@linux.vnet.ibm.com

--- tests/test_server.py
+++ tests/test_server.py
@@ -33,8 +33,7 @@ import burnet.mockmodel
 class ServerTests(unittest.TestCase):
     def test_server_start(self):
         """
-        Test that we can start a server and receive a response.  Right now we
-        have no content so we expect HTTP:404
+        Test that we can start a server and receive HTTP:200.
         """
         host = '127.0.0.1'
         port = utils.get_free_port()
@@ -42,9 +41,7 @@ class ServerTests(unittest.TestCase):
         s = utils.run_server(host, port, test_mode=True, model=model)
         try:
             resp = utils.request(host, port, '/')
-            data = json.loads(resp.read())
             self.assertEquals(200, resp.status)
-            self.assertEquals('localhost', data['hostname'])
         except:
             raise
         finally:
