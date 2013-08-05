49a02da39509b9d17a78dfd4d2b677b8b26007b7
Hongliang Wang <hlwang@linux.vnet.ibm.com>
2013-08-05 18:03:55 +0800

    Issue #72: Make Pages Be Bookmark-able
    V2 -> V3:
        Remove changes to i18n-related code.
    
    V1 -> V2:
        Rebase to master branch as B[b]urnet renamed to K[k]imchi.
    
    Pages are supposed bookmark-able and not always take the user to
    the main page even if the user want to see template page directly.
    Use location.hash to enable the feature, e.g., user can type in
    URL http://kimchi.company.com:8000/#template to see template list
    directly. Then user can use browser Back/Forward Buttons as well.
    
        Tested on IE9, IE10, Safari, Firefox 22, Chrome.
    
    Signed-off-by: Hongliang Wang <hlwang@linux.vnet.ibm.com>
    Reviewed-by: Meina Zhou <zhoumein@linux.vnet.ibm.com>
    Reviewed-by: Aline Manera <alinefm@br.ibm.com>

--- ui/js/src/kimchi.main.js
+++ ui/js/src/kimchi.main.js
@@ -4,7 +4,7 @@
  * Copyright IBM, Corp. 2013
  *
  * Authors:
- *  Hongliang Wang <hlwanghl@cn.ibm.com>
+ *  Hongliang Wang <hlwang@linux.vnet.ibm.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,26 +19,83 @@
  * limitations under the License.
  */
 kimchi.main = function() {
-	kimchi.popable();
+    kimchi.popable();
 
-	$('#nav-menu a.item').on('click', function() {
-	    var href = $(this).attr('href');
-	    kimchi.vmTimeout && clearTimeout(kimchi.vmTimeout);
-	    $('#main').load(href);
-	    var left = $(this).parent().position().left;
-	    var width = $(this).parent().width();
-	    $('.menu-arrow').stop().animate({
-		left: left+width/2-10
-	    });
-	    return false;
-	});
+    /**
+     * Use Ajax to dynamically load a page without a page refreshing.
+     * Handle arrow cursor animation, DOM node focus, and page content
+     * rendering.
+     */
+    var loadPage = function(url) {
+        /*
+         * We setup an periodly reloading of VM list, which should be
+         * removed when switching to non-guest pages.
+         */
+        kimchi.vmTimeout && clearTimeout(kimchi.vmTimeout);
 
-	var currentMenu = $('.nav-menu .item.current');
-	var left = currentMenu.parent().position().left;
-	var width = currentMenu.parent().width();
-	$('.menu-arrow').css('left', left+width/2-10);
+        // Get the page content through Ajax and render it.
+        $('#main').load(url);
 
-	$('#main').load('i18n.html', function() {
-	    $(this).load('guestTab.html');
-	});
+        /*
+         * We use the HTML file name for hash, like:
+         * guestTab for guestTab.html, template for template.html.
+         * Retrieve hash from the given URL and update URL hash
+         * value to put an item in browsing history to make pages
+         * be bookmark-able.
+         */
+        var hashString = url.substring(0, url.length - 5);
+        location.hash = hashString;
+
+        /*
+         * Find the corresponding tab DOM node and animate the arrow
+         * cursor to point to the tab.
+         */
+        var tab = $('#nav-menu a[href="' + url + '"]');
+        var left = $(tab).parent().position().left;
+        var width = $(tab).parent().width();
+        $('.menu-arrow').stop().animate({
+            left: left + width / 2 - 10
+        });
+
+        // Update the visual style of tabs; focus the selected one.
+        $('#nav-menu a').removeClass('current');
+        $(tab).addClass('current');
+        $(tab).focus();
+    };
+
+    /*
+     * Register click listener of tabs. Replace the default reloading
+     * page behavior of <a> with Ajax loading.
+     */
+    $('#nav-menu a.item').on('click', function() {
+        loadPage($(this).attr('href'));
+        return false;
+    });
+
+    /*
+     * If hash value is changed, then we know the user is intended to
+     * load another page.
+     */
+    window.onhashchange = function() {
+        var hashString = location.hash.substr(1);
+        if(!hashString) {
+            return;
+        }
+
+        var url = hashString + '.html';
+        loadPage(url);
+    };
+
+    // Load i18n translation strings first and then render the page.
+    $('#main').load('i18n.html', function() {
+        /*
+         * Initialize page content.
+         * 1) If user types in the main page URL without hash, then we load
+         *    VM list page by default, e.g., http://kimchi.company.com:8000;
+         * 2) If user types a URL with hash, we load that page, e.g.,
+         *    http://kimchi.company.com:8000/#template.
+         */
+        var hashString = (location.hash && location.hash.substr(1)) || 'guestTab';
+        loadPage(hashString + '.html');
+    });
 };
