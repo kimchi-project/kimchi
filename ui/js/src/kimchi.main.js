4ea86e3b9b539c1c99d9fc423cf11f95dc0f4e5d
Hongliang Wang <hlwang@linux.vnet.ibm.com>
2013-09-24 18:58:43 +0800

    Issue #167: Duplicate Requests for HTML Fragment when Switching Pages
    There're duplicate request when switching pages. It's caused by the
    wrong code in onKimchiRedirect() function in which location.hash is
    set again so onhashchange event will be triggered again.
    
    Signed-off-by: Hongliang Wang <hlwang@linux.vnet.ibm.com>

--- ui/js/src/kimchi.main.js
+++ ui/js/src/kimchi.main.js
@@ -26,8 +26,12 @@ kimchi.main = function() {
     /**
      * Do the following setup:
      *   1) Clear any timing events.
-     *   2) Update URL hash to new page.
-     *   3) Update page tabs indicator.
+     *   2) If the given URL is invalid (i.e., no corresponding href value in
+     *      page tab list.), then clear location.href and inform the user;
+     *
+     *      Or else:
+     *      Move the page tab indicator to the right position;
+     *      Load the page content via Ajax.
      */
     var onKimchiRedirect = function(url) {
         /*
@@ -37,26 +41,18 @@ kimchi.main = function() {
         kimchi.vmTimeout && clearTimeout(kimchi.vmTimeout);
 
         /*
-         * We use the HTML file name for hash, like: guests for guests.html,
-         * templates for templates.html. Retrieve hash from the given URL and
-         * update URL hash value to put an item in browsing history to make
-         * pages be bookmark-able.
-         */
-        var hashString = url.substring(0, url.length - 5);
-
-        if (location.hash != hashString) {
-            location.hash = hashString;
-        }
-
-        /*
-         * Find the corresponding tab DOM node and animate the arrow cursor to
-         * point to the tab.
+         * Find the corresponding tab node and animate the arrow indicator to
+         * point to the tab. If nothing found, inform user the URL is invalid
+         * and clear location.hash to jump to home page.
          */
         var tab = $('#nav-menu a[href="' + url + '"]');
         if (tab.length === 0) {
+            kimchi.message.error(i18n['msg.err.uri.invalid']);
+            location.hash = '';
             return;
         }
 
+        // Animate arrow indicator.
         var left = $(tab).parent().position().left;
         var width = $(tab).parent().width();
         $('.menu-arrow').stop().animate({
@@ -67,6 +63,9 @@ kimchi.main = function() {
         $('#nav-menu a').removeClass('current');
         $(tab).addClass('current');
         $(tab).focus();
+
+        // Load page content.
+        loadPage(url);
     };
 
     /**
@@ -74,36 +73,40 @@ kimchi.main = function() {
      * arrow cursor animation, DOM node focus, and page content rendering.
      */
     var loadPage = function(url) {
-        if (0 === $('#nav-menu a[href="' + url + '"]').length) {
-            kimchi.message.error(i18n['msg.err.uri.invalid']);
-            location.hash = '';
-            return;
-        }
-
-        kimchi.topic('redirect').publish(url);
-
         // Get the page content through Ajax and render it.
         $('#main').load(url, function(responseText, textStatus, jqXHR) {});
     };
 
+    /*
+     * Update page content.
+     * 1) If user types in the main page URL without hash, then we apply the
+     *    default hash. e.g., http://kimchi.company.com:8000;
+     * 2) If user types a URL with hash, then we publish an "redirect" event
+     *    to load the page, e.g., http://kimchi.company.com:8000/#templates.
+     */
     var updatePage = function() {
-        /*
-         * Update page content.
-         * 1) If user types in the main page URL without hash, then we load
-         *    VM list page by default, e.g., http://kimchi.company.com:8000;
-         * 2) If user types a URL with hash, we load that page, e.g.,
-         *    http://kimchi.company.com:8000/#template.
-         */
+        // Parse hash string.
         var hashString = (location.hash && location.hash.substr(1));
 
+        /*
+         * If hash string is empty, then apply the default one;
+         * or else, publish an "redirect" event to load the page.
+         */
         if (!hashString) {
             location.hash = DEFAULT_HASH;
         }
         else {
-            loadPage(hashString + '.html');
+            kimchi.topic('redirect').publish(hashString + '.html');
         }
     };
 
+    /**
+     * Register listeners including:
+     * 1) Kimchi redirect event
+     * 2) hashchange event
+     * 3) Tab list click event
+     * 4) Log-out button click event
+     */
     var initListeners = function() {
         kimchi.topic('redirect').subscribe(onKimchiRedirect);
 
@@ -111,21 +114,31 @@ kimchi.main = function() {
          * If hash value is changed, then we know the user is intended to load
          * another page.
          */
-        window.onhashchange = function() {
-            updatePage();
-        };
+        window.onhashchange = updatePage;
 
         /*
          * Register click listener of tabs. Replace the default reloading page
          * behavior of <a> with Ajax loading.
          */
         $('#nav-menu a.item').on('click', function(event) {
-            loadPage($(this).attr('href'));
+            var url = $(this).attr('href');
+
+            /*
+             * We use the HTML file name for hash, like: guests for guests.html
+             * and templates for templates.html.
+             *     Retrieve hash value from the given URL and update location's
+             * hash part. It has 2 effects: one is to publish Kimchi "redirect"
+             * event to trigger listener, the other is to put an entry into the
+             * browser's address history to make pages be bookmark-able.
+             */
+            var hashString = url.substring(0, url.length - 5);
+            location.hash = hashString;
 
             // Prevent <a> causing browser redirecting to other page.
             event.preventDefault();
         });
 
+        // Perform logging out via Ajax request.
         $('#btn-logout').on('click', function() {
             kimchi.logout(function() {
                 updatePage();
