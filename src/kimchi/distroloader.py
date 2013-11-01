7add3d9c1e4e98cf5c74739c2330cf151e9440e6
ShaoHe Feng <shaohef@linux.vnet.ibm.com>
2013-11-01 20:25:22 +0800

    distros: add a DistroLoader to load distros
    The distro iso links will be stored in /etc/kimchi/distros.d/
    
    A basic DistroLoader class that reads all of the files and produces
    a python array identical to the osinfo array.
    
    Adam Litke suggested:
    The distros files cannot be updated at runtime.
    The only thing kimchi needs to do with the files in /etc/kimchi/distros.d
    is read them to create a large json list of template objects.
    It will never write to those files.
    
    user can create a file in /etc/kimchi/distros.d by installing / upgrading
    a package
    
    Signed-off-by: ShaoHe Feng <shaohef@linux.vnet.ibm.com>
    Reviewed-by: Aline Manera <alinefm@linux.vnet.ibm.com>

---
+++ src/kimchi/distroloader.py
@@ -0,0 +1,56 @@
+#
+# Project Kimchi
+#
+# Copyright IBM, Corp. 2013
+#
+# Authors:
+#  ShaoHe Feng <shaohef@linux.vnet.ibm.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+import json
+import os
+import config
+import glob
+from kimchi.exception import *
+from kimchi.utils import kimchi_log
+
+class DistroLoader(object):
+
+    def __init__(self, location=None):
+        self.location = location or config.get_distros_store()
+
+    def _get_json_info(self, fname):
+        if not os.path.isfile(fname):
+            msg = "DistroLoader: failed to find distro file: %s" % fname
+            kimchi_log.error(msg)
+            raise NotFoundError(msg)
+        try:
+            with open(fname) as f:
+                data = json.load(f)
+            return data
+        except ValueError:
+            msg = "DistroLoader: failed to parse json from distro file: %s" % fname
+            kimchi_log.error(msg)
+            raise OperationFailed(msg)
+
+    def get(self):
+        all_json_files = glob.glob("%s/%s" % (self.location, "*.json"))
+        distros = []
+        for f in all_json_files:
+            distros.extend(self._get_json_info(f))
+
+        return dict([(distro['name'], distro) for distro in distros])
