0469f1eceed7d18e4dbdb29580829754e13de10f
Royce Lv <lvroyce@linux.vnet.ibm.com>
2013-11-14 17:45:38 +0800

    Deep scan: Add functions for deep scan preparation
    Prepare the temp directory for pool and create then symlink,
    implement the scan logic with probe_iso.
    pool timeout check will be done when a new scan request is issued,
    timeout scan result clean includes clean storage pool,
    directory and db entries.
    
    Signed-off-by: Royce Lv <lvroyce@linux.vnet.ibm.com>

---
+++ src/kimchi/scan.py
@@ -0,0 +1,53 @@
+import glob
+import shutil
+import tempfile
+import os.path
+import time
+import uuid
+
+from kimchi.isoinfo import probe_iso
+from utils import kimchi_log
+
+
+class Scanner(object):
+    SCAN_TTL = 300
+
+    def __init__(self, record_clean_cb):
+        self.clean_cb = record_clean_cb
+
+    def delete(self):
+        self.clean_stale(-1)
+
+    def clean_stale(self, window=SCAN_TTL):
+        """
+        Clear scan pools generated before time window,
+        Clear all scan pools if window is -1.
+        """
+        try:
+            now = time.time()
+            clean_list = glob.glob("/tmp/kimchi-scan-*")
+            for d in clean_list:
+                transient_pool = \
+                    os.path.basename(d).replace('kimchi-scan-', '')[0: -6]
+                if now - os.path.getmtime(d) > window:
+                    shutil.rmtree(d)
+                    self.clean_cb(transient_pool)
+        except OSError as e:
+            kimchi_log.debug(
+                    "Exception %s occured when cleaning stale pool, ignore" % e.message)
+
+    def scan_dir_prepare(self, name, path='/'):
+        # clean stale scan storage pools
+        self.clean_stale()
+        return tempfile.mkdtemp(prefix='kimchi-scan-' + name, dir='/tmp')
+
+    def start_scan(self, cb, params):
+        def updater(iso_info):
+            link_name = os.path.join(
+                params['pool_path'],
+                os.path.basename(iso_info['path']) + '.' + str(uuid.uuid4()))
+            os.symlink(iso_info['path'], link_name)
+
+        scan_params = dict(path=params['scan_path'], updater=updater)
+        probe_iso(None, scan_params)
+        cb('', True)
