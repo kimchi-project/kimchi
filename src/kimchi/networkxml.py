d30ca35f6ea444932d032f84aa67df08d5e837af
ShaoHe Feng <shaohef@linux.vnet.ibm.com>
2013-12-01 14:27:40 +0800

    network: generate network xml and testcast for it
    Define a network xml template.
    Also a test case for this xml template.
    
    info: http://libvirt.org/formatnetwork.html
    
    Signed-off-by: ShaoHe Feng <shaohef@linux.vnet.ibm.com>

---
+++ src/kimchi/networkxml.py
@@ -0,0 +1,111 @@
+#
+# Project Kimchi
+#
+# Copyright IBM, Corp. 2013
+#
+# Authors:
+#  ShaoHe Feng <shaohef@linux.vnet.ibm.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import ipaddr
+
+
+# FIXME, do not support ipv6
+
+def _get_dhcp_xml(**kwargs):
+    """
+    <dhcp>
+      <range start="192.168.122.100" end="192.168.122.254" />
+      <host mac="00:16:3e:77:e2:ed" name="foo.test.com" ip="192.168.122.10" />
+      <host mac="00:16:3e:3e:a9:1a" name="bar.test.com" ip="192.168.122.11" />
+    </dhcp>
+    """
+    xml = ''
+    dhcp_range = "  <range start='%(start)s' end='%(end)s' />"
+    ipv4host = "  <host mac='%(mac)s' name='%(name)s' ip='%(ip)s' />"
+    dhcp = []
+    if 'range' in kwargs.keys():
+        dhcp.append(dhcp_range % kwargs['range'])
+    if 'hosts' in kwargs.keys():
+        dhcp.extend([ipv4host % host for host in kwargs['hosts']])
+    if dhcp:
+        xml = "\n".join(["<dhcp>"] + dhcp + ["</dhcp>"])
+    return xml
+
+
+def _get_ip_xml(**kwargs):
+    """
+    <ip address="192.168.152.1" netmask="255.255.255.0">
+      <dhcp>
+        <range start="192.168.152.2" end="192.168.152.254" />
+      </dhcp>
+    </ip>
+    """
+    xml = ""
+    if 'net' in kwargs.keys():
+        net = ipaddr.IPNetwork(kwargs['net'])
+        address = str(net.network)
+        netmask = str(net.netmask)
+        dhcp_params = kwargs.get('dhcp', {})
+        dhcp = _get_dhcp_xml(**dhcp_params)
+        xml = """
+          <ip address='%s' netmask='%s'>"
+            %s
+          </ip>""" % (address, netmask, dhcp)
+    return xml
+
+
+def _get_forward_xml(**kwargs):
+    """
+    <forward mode='hostdev' dev='eth0' managed='yes'>
+    </forward>
+    """
+
+    if "mode" in kwargs.keys() and kwargs['mode'] is None:
+        return ""
+    mode = " mode='%s'" % kwargs['mode'] if 'mode' in kwargs.keys() else ""
+    dev = " dev='%s'" % kwargs['dev'] if 'dev' in kwargs.keys() else ""
+    managed = (" managed='%s'" % kwargs['managed']
+               if 'managed' in kwargs.keys() else "")
+    xml = """
+      <forward %s%s%s>
+      </forward>
+    """ % (mode, dev, managed)
+    return xml
+
+
+def to_network_xml(**kwargs):
+
+    params = {'name': kwargs['name']}
+    # None means is Isolated network, {} means default mode nat
+    forward = kwargs.get('forward', {"mode": None})
+    ip = {'net': kwargs['net']} if 'net' in kwargs else {}
+    ip['dhcp'] = kwargs.get('dhcp', {})
+    bridge = kwargs.get('bridge')
+    params = {'name': kwargs['name'],
+              'forward': _get_forward_xml(**forward),
+              'bridge': "<bridge name='%s' />" % bridge if bridge else "",
+              'ip': _get_ip_xml(**ip)}
+
+    xml = """
+    <network>
+      <name>%(name)s</name>
+        %(bridge)s
+        %(forward)s
+        %(ip)s
+    </network>
+    """ % params
+    return xml
---
+++ tests/test_networkxml.py
@@ -0,0 +1,151 @@
+#
+# Kimchi
+#
+# Copyright IBM Corp, 2013
+#
+# Authors:
+#  ShaoHe Feng <shaohef@linux.vnet.ibm.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import unittest
+import kimchi.networkxml as nxml
+from kimchi.xmlutils import xpath_get_text
+import ipaddr
+
+
+class NetworkXmlTests(unittest.TestCase):
+    def test_dhcp_xml(self):
+        """
+        Test network dhcp xml
+        """
+        dhcp_range = {"start": "192.168.122.100", "end": "192.168.122.254"}
+        host1 = {"mac": "00:16:3e:77:e2:ed",
+                 "name": "foo.example.com",
+                 "ip": "192.168.122.10"}
+        host2 = {"mac": "00:16:3e:3e:a9:1a",
+                 "name": "bar.example.com",
+                 "ip": "192.168.122.11"}
+        params = {}
+
+        xml = nxml._get_dhcp_xml(**params)
+        self.assertEquals("", xml)
+
+        params["range"] = dhcp_range
+        xml = nxml._get_dhcp_xml(**params)
+        start = xpath_get_text(xml, "/dhcp/range/@start")
+        end = xpath_get_text(xml, "/dhcp/range/@end")
+        self.assertEquals(dhcp_range['start'], start[0])
+        self.assertEquals(dhcp_range['end'], end[0])
+
+        params["hosts"] = [host1, host2]
+        xml = nxml._get_dhcp_xml(**params)
+        ip = xpath_get_text(xml, "/dhcp/host/@ip")
+        self.assertEquals(ip, [host1['ip'], host2['ip']])
+
+    def test_ip_xml(self):
+        """
+        Test network ip xml
+        """
+        dhcp_range = {"start": "192.168.122.100", "end": "192.168.122.254"}
+        params = {}
+
+        xml = nxml._get_dhcp_xml(**params)
+        self.assertEquals("", xml)
+
+        params["net"] = "192.168.122.0/255.255.255.0"
+        params["dhcp"] = {'range': dhcp_range}
+        xml = nxml._get_ip_xml(**params)
+        start = xpath_get_text(xml, "/ip/dhcp/range/@start")[0]
+        end = xpath_get_text(xml, "/ip/dhcp/range/@end")[0]
+        self.assertEquals(dhcp_range['start'], start)
+        self.assertEquals(dhcp_range['end'], end)
+
+        address = xpath_get_text(xml, "/ip/@address")[0]
+        netmask = xpath_get_text(xml, "/ip/@netmask")[0]
+        self.assertEquals(address, params["net"].split("/")[0])
+        self.assertEquals(netmask, params["net"].split("/")[1])
+
+        # test _get_ip_xml can accepts strings: '192.168.122.0/24',
+        # which is same as "192.168.122.0/255.255.255.0"
+        params["net"] = "192.168.122.0/24"
+        xml = nxml._get_ip_xml(**params)
+        netmask = xpath_get_text(xml, "/ip/@netmask")[0]
+        self.assertEquals(netmask,
+                          str(ipaddr.IPNetwork(params["net"]).netmask))
+
+    def test_forward_xml(self):
+        """
+        Test network forward xml
+        """
+        params = {"mode": None}
+
+        xml = nxml._get_forward_xml(**params)
+        self.assertEquals("", xml)
+
+        params["mode"] = 'nat'
+        params["dev"] = 'eth0'
+        xml = nxml._get_forward_xml(**params)
+        mode = xpath_get_text(xml, "/forward/@mode")[0]
+        dev = xpath_get_text(xml, "/forward/@dev")[0]
+        self.assertEquals(params['mode'], mode)
+        self.assertEquals(params['dev'], dev)
+
+    def test_network_xml(self):
+        """
+        Test network xml
+        """
+        params = {"name": "test",
+                  "forward": {"mode": "nat", "dev": ""},
+                  "net": "192.168.0.0/255.255.255.0"}
+        xml = nxml.to_network_xml(**params)
+        name = xpath_get_text(xml, "/network/name")[0]
+        self.assertEquals(name, params['name'])
+
+        forward_mode = xpath_get_text(xml, "/network/forward/@mode")[0]
+        self.assertEquals(forward_mode, params['forward']['mode'])
+        forward_dev = xpath_get_text(xml, "/network/forward/@dev")[0]
+        self.assertEquals(forward_dev, '')
+
+        address = xpath_get_text(xml, "/network/ip/@address")[0]
+        self.assertEquals(address, params["net"].split("/")[0])
+        netmask = xpath_get_text(xml, "/network/ip/@netmask")[0]
+        self.assertEquals(netmask, params["net"].split("/")[1])
+
+        dhcp_start = xpath_get_text(xml, "/network/ip/dhcp/range/@start")
+        self.assertEquals(dhcp_start, [])
+        dhcp_end = xpath_get_text(xml, "/network/ip/dhcp/range/@end")
+        self.assertEquals(dhcp_end, [])
+
+        # test optional params
+        params['forward']['dev'] = "eth0"
+        params['dhcp'] = {"range": {'start': '192.168.0.1',
+                                    'end': '192.168.0.254'}}
+        xml = nxml.to_network_xml(**params)
+        forward_dev = xpath_get_text(xml, "/network/forward/@dev")[0]
+        self.assertEquals(forward_dev, params['forward']['dev'])
+
+        dhcp_start = xpath_get_text(xml, "/network/ip/dhcp/range/@start")[0]
+        self.assertEquals(dhcp_start, params['dhcp']['range']['start'])
+        dhcp_end = xpath_get_text(xml, "/network/ip/dhcp/range/@end")[0]
+        self.assertEquals(dhcp_end, params['dhcp']['range']['end'])
+
+        # test _get_ip_xml can accepts strings: '192.168.122.0/24',
+        # which is same as "192.168.122.0/255.255.255.0"
+        params["net"] = "192.168.0.0/24"
+        xml = nxml.to_network_xml(**params)
+        netmask = xpath_get_text(xml, "/network/ip/@netmask")[0]
+        self.assertEquals(netmask,
+                          str(ipaddr.IPNetwork(params["net"]).netmask))
