be26ed4ffa41f5fc0d490237ff55fe850454bce5
Aline Manera <alinefm@br.ibm.com>
2013-12-27 17:47:50 -0200

    pep8 cleanup for distroloader.py
    This patch cleans up pep8 style issue in distroloader.py
    
    Signed-off-by: Aline Manera <alinefm@br.ibm.com>

--- src/kimchi/distroloader.py
+++ src/kimchi/distroloader.py
@@ -18,7 +18,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 #
 
 import glob
@@ -46,7 +46,7 @@ class DistroLoader(object):
                 data = json.load(f)
             return data
         except ValueError:
-            msg = "DistroLoader: failed to parse json from distro file: %s" % fname
+            msg = "DistroLoader: failed to parse distro file: %s" % fname
             kimchi_log.error(msg)
             raise OperationFailed(msg)
 
