1a0fd41b869c366b621254af9646c8135ad750e7
Aline Manera <alinefm@br.ibm.com>
2013-12-27 17:52:43 -0200

    pep8 cleanup for exception.py
    This patch cleans up pep8 style issue in exception.py
    
    Signed-off-by: Aline Manera <alinefm@br.ibm.com>

--- src/kimchi/exception.py
+++ src/kimchi/exception.py
@@ -18,19 +18,24 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
 
 class NotFoundError(Exception):
     pass
 
+
 class OperationFailed(Exception):
     pass
+
 
 class MissingParameter(Exception):
     pass
 
+
 class InvalidParameter(Exception):
     pass
+
 
 class InvalidOperation(Exception):
     pass
