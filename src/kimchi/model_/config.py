39d1b7b988311ebbe9a998edb15b0959c2233d0c
Aline Manera <alinefm@br.ibm.com>
2014-01-28 17:30:15 -0200

    refactor model: Create a separated  model for config resource
    The model implementation for config and its sub-resources were added to
    model_/config.py
    It is needed to create a temporary module named model_ as there is
    already a model.py in Kimchi code.
    It will be rename later when removing model.py
    
    Signed-off-by: Aline Manera <alinefm@br.ibm.com>

---
+++ src/kimchi/model_/config.py
@@ -0,0 +1,98 @@
+#
+# Project Kimchi
+#
+# Copyright IBM, Corp. 2013
+#
+# Authors:
+#  Aline Manera <alinefm@linux.vnet.ibm.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+import cherrypy
+
+from kimchi.basemodel import Singleton
+from kimchi.config import config as kconfig
+from kimchi.distroloader import DistroLoader
+from kimchi.exception import NotFoundError
+from kimchi.featuretests import FeatureTests
+from kimchi.model_.debugreports import DebugReportsModel
+from kimchi.screenshot import VMScreenshot
+from kimchi.utils import kimchi_log
+
+
+class ConfigModel(object):
+    def __init__(self, **kargs):
+        pass
+
+    def lookup(self, name):
+        proxy_port = kconfig.get('display', 'display_proxy_port')
+        return {'http_port': cherrypy.server.socket_port,
+                'display_proxy_port': proxy_port}
+
+
+class CapabilitiesModel(object):
+    __metaclass__ = Singleton
+
+    def __init__(self, **kargs):
+        self.qemu_stream = False
+        self.qemu_stream_dns = False
+        self.libvirt_stream_protocols = []
+
+        # Subscribe function to set host capabilities to be run when cherrypy
+        # server is up
+        # It is needed because some features tests depends on the server
+        cherrypy.engine.subscribe('start', self._set_capabilities)
+
+    def _set_capabilities(self):
+        kimchi_log.info("*** Running feature tests ***")
+        self.qemu_stream = FeatureTests.qemu_supports_iso_stream()
+        self.qemu_stream_dns = FeatureTests.qemu_iso_stream_dns()
+        self.nfs_target_probe = FeatureTests.libvirt_support_nfs_probe()
+
+        self.libvirt_stream_protocols = []
+        for p in ['http', 'https', 'ftp', 'ftps', 'tftp']:
+            if FeatureTests.libvirt_supports_iso_stream(p):
+                self.libvirt_stream_protocols.append(p)
+
+        kimchi_log.info("*** Feature tests completed ***")
+    _set_capabilities.priority = 90
+
+    def lookup(self, *ident):
+        report_tool = DebugReportsModel.get_system_report_tool()
+
+        return {'libvirt_stream_protocols': self.libvirt_stream_protocols,
+                'qemu_stream': self.qemu_stream,
+                'screenshot': VMScreenshot.get_stream_test_result(),
+                'system_report_tool': bool(report_tool)}
+
+
+class DistrosModel(object):
+    def __init__(self, **kargs):
+        distroloader = DistroLoader()
+        self.distros = distroloader.get()
+
+    def get_list(self):
+        return self.distros.keys()
+
+
+class DistroModel(object):
+    def __init__(self, **kargs):
+        self._distros = DistrosModel()
+
+    def lookup(self, name):
+        try:
+            return self._distros.distros[name]
+        except KeyError:
+            raise NotFoundError("Distro '%s' not found." % name)
