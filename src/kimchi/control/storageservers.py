de892cbcdbfffe71eb872411de46f92b7acce7d3
Aline Manera <alinefm@br.ibm.com>
2014-01-28 18:28:03 -0200

    bug fix: Expose /storageservers
    Now to expose a new uri for kimchi server we need to use the UrlSubNode
    decorator, otherwise the uri will not be available.
    
    Signed-off-by: Aline Manera <alinefm@br.ibm.com>

--- src/kimchi/control/storageservers.py
+++ src/kimchi/control/storageservers.py
@@ -20,11 +20,12 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
-from kimchi.control.base import Collection, Resource
-from kimchi.control.utils import get_class_name, model_fn
 import kimchi.template
+from kimchi.control.base import Collection, Resource
+from kimchi.control.utils import get_class_name, model_fn, UrlSubNode
 
 
+@UrlSubNode("storageservers", True)
 class StorageServers(Collection):
     def __init__(self, model):
         super(StorageServers, self).__init__(model)
