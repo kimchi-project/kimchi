c08c091e3168782a08f8b15812fdcaa9fe6ea174
Aline Manera <alinefm@br.ibm.com>
2014-01-28 17:08:02 -0200

    refactor model: Create a separated  model for task resource
    The model implementation for config and its sub-resources were added to
    model_/tasks.py
    
    Signed-off-by: Aline Manera <alinefm@br.ibm.com>

---
+++ src/kimchi/model_/tasks.py
@@ -0,0 +1,39 @@
+#
+# Project Kimchi
+#
+# Copyright IBM, Corp. 2013
+#
+# Authors:
+#  Aline Manera <alinefm@linux.vnet.ibm.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+
+class TasksModel(object):
+    def __init__(self, **kargs):
+        self.objstore = kargs['objstore']
+
+    def get_list(self):
+        with self.objstore as session:
+            return session.get_list('task')
+
+
+class TaskModel(object):
+    def __init__(self, **kargs):
+        self.objstore = kargs['objstore']
+
+    def lookup(self, id):
+        with self.objstore as session:
+            return session.get('task', str(id))
