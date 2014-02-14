68701c21a8036c9e0d5ec5ab6eac634dde137696
Rodrigo Trujillo <rodrigo.trujillo@linux.vnet.ibm.com>
2014-02-13 23:45:35 -0200

    CDROM Management: Update controller and API.json for guest storages
    This patch adds the control classes for guest storages
    
    Signed-off-by: Royce Lv <lvroyce@linux.vnet.ibm.com>
    Signed-off-by: Rodrigo Trujillo <rodrigo.trujillo@linux.vnet.ibm.com>
    
    Removing hard disk support from API.json
    Changed API.json declarations to the new exception model
    
    Signed-off-by: Daniel Henrique Barboza <danielhb@linux.vnet.ibm.com>

---
+++ src/kimchi/control/vm/storages.py
@@ -0,0 +1,49 @@
+#
+# Project Kimchi
+#
+# Copyright IBM, Corp. 2014
+#
+# Authors:
+#  Rodrigo Trujilo <rodrigo.trujillo@linux.vnet.ibm.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+from kimchi.control.base import Collection, Resource
+from kimchi.control.utils import UrlSubNode
+
+
+@UrlSubNode("storages")
+class VMStorages(Collection):
+    def __init__(self, model, vm):
+        super(VMStorages, self).__init__(model)
+        self.resource = VMStorage
+        self.vm = vm
+        self.resource_args = [self.vm, ]
+        self.model_args = [self.vm, ]
+
+
+class VMStorage(Resource):
+    def __init__(self, model, vm, ident):
+        super(VMStorage, self).__init__(model, ident)
+        self.vm = vm
+        self.ident = ident
+        self.info = {}
+        self.model_args = [self.vm, self.ident]
+        self.uri_fmt = '/vms/%s/storages/%s'
+        self.update_params = ['path']
+
+    @property
+    def data(self):
+        return self.info
