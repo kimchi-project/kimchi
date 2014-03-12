39a2ba82d2b66bd433bb3cb7789946786134d6be
ShaoHe Feng <shaohef@linux.vnet.ibm.com>
2014-03-12 13:06:12 +0800

    move _default_network_check from top model to networks model
    Top model should load other model and structure preparing.
    
    The default network check is network function, move to networks model
    is more reasonable.
    
    And the important: it is more convenient to call attr of networks model
    
    Signed-off-by: ShaoHe Feng <shaohef@linux.vnet.ibm.com>

--- src/kimchi/model/networks.py
+++ src/kimchi/model/networks.py
@@ -32,6 +32,42 @@ from kimchi.rollbackcontext import RollbackContext
 class NetworksModel(object):
     def __init__(self, **kargs):
         self.conn = kargs['conn']
+        if 'qemu:///' in self.conn.get().getURI():
+            self._default_network_check()
+
+    def _default_network_check(self):
+        conn = self.conn.get()
+        xml = """
+            <network>
+              <name>default</name>
+              <forward mode='nat'/>
+              <bridge name='virbr0' stp='on' delay='0' />
+              <ip address='192.168.122.1' netmask='255.255.255.0'>
+                <dhcp>
+                  <range start='192.168.122.2' end='192.168.122.254' />
+                </dhcp>
+              </ip>
+            </network>
+        """
+        try:
+            net = conn.networkLookupByName("default")
+        except libvirt.libvirtError:
+            try:
+                net = conn.networkDefineXML(xml)
+            except libvirt.libvirtError, e:
+                cherrypy.log.error("Fatal: Cannot create default network "
+                                   "because of %s, exit kimchid" % e.message,
+                                   severity=logging.ERROR)
+                sys.exit(1)
+
+        if net.isActive() == 0:
+            try:
+                net.create()
+            except libvirt.libvirtError, e:
+                cherrypy.log.error("Fatal: Cannot activate default network "
+                                   "because of %s, exit kimchid" % e.message,
+                                   severity=logging.ERROR)
+                sys.exit(1)
 
     def create(self, params):
         conn = self.conn.get()
