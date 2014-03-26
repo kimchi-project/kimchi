799910280a13775063ba99c44d1e09f9b51e75dd
Hongliang Wang <hlwang@linux.vnet.ibm.com>
2014-03-25 22:57:56 -0400

    Repository Management - Edit Repository Support
    Edit repository support.
    
    Signed-off-by: Hongliang Wang <hlwang@linux.vnet.ibm.com>
    Signed-off-by: Adam King <rak@linux.vnet.ibm.com>

---
+++ ui/js/src/kimchi.repository_edit_main.js
@@ -0,0 +1,104 @@
+/*
+ * Project Kimchi
+ *
+ * Copyright IBM, Corp. 2013
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+kimchi.repository_fillForm = function(form, name, values) {
+    var name= (name=="" || !name) ? "%s" : name;
+    for(var prop in values) {
+        if ((typeof(values[prop])==="object") && !Array.isArray(values[prop])) {
+            tmpName=name.replace("%s", prop+"[%s]" );
+            kimchi.repository_fillForm(form, tmpName, values[prop]);
+        }
+        else {
+            tmpName=name.replace("%s", prop );
+            var control = $('input[name="' + tmpName + '"]', form);
+            switch($(control).attr('type')) {
+            case 'text':
+                $(control).val(values[prop]);
+                break;
+            case 'radio':
+            case 'checkbox':
+                $(control).each(function(i, c) {
+                    var matched = ('' + values[prop]) == $(c).val();
+                    $(c).prop('checked', matched);
+                });
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
+
+kimchi.repository_edit_main = function() {
+
+    var editForm = $('#form-repository-edit');
+
+    var saveButton = $('#repository-edit-button-save');
+
+    if(kimchi.host.capabilities['repo_mngt_tool']=="yum") {
+        editForm.find('input.deb').prop('disabled', true);
+    }
+    else if(kimchi.host.capabilities['repo_mngt_tool']=="deb") {
+        editForm.find('input.yum').prop('disabled', true);
+    }
+
+    kimchi.retrieveRepository(kimchi.selectedRepository, function(repository) {
+        kimchi.repository_fillForm(editForm,"",repository);
+
+        $('input', editForm).on('input propertychange', function(event) {
+            if($(this).val() !== '') {
+                $(saveButton).prop('disabled', false);
+            }
+        });
+    });
+
+
+    var editRepository = function(event) {
+        var formData = $(editForm).serializeObject();
+
+        if (formData && formData.config) {
+            formData.config.gpgcheck=(String(formData.config.gpgcheck).toLowerCase() === 'true');
+        }
+
+        if(formData.config && formData.config.comps) {
+            formData.config.comps=formData.config.comps.split(/[,\s]/);
+            for(var i=0; i>formData.config.comps.length; i++) {
+                formData.config.comps[i]=formData.config.comps[i].trim();
+            }
+            for (var j=formData.config.comps.indexOf(""); j!=-1; j=formData.config.comps.indexOf("")) {
+                formData.config.comps.splice(j, 1);
+            }
+        }
+
+        kimchi.updateRepository(kimchi.selectedRepository, formData, function() {
+            kimchi.topic('kimchi/repositoryUpdated').publish();
+            kimchi.window.close();
+        }, function(jqXHR, textStatus, errorThrown) {
+            var reason = jqXHR &&
+                jqXHR['responseJSON'] &&
+                jqXHR['responseJSON']['reason'];
+            reason = reason ? ': ' + reason : '';
+            kimchi.message.error(i18n['KCHREPO6015M'] + reason);
+        });
+
+        return false;
+    };
+
+    $(editForm).on('submit', editRepository);
+    $(saveButton).on('click', editRepository);
+};
