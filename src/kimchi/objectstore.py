5252d7ae89fcb5c4e3aa2bd1df7d3509f72b2008
Rodrigo Trujillo <rodrigo.trujillo@linux.vnet.ibm.com>
2014-04-02 16:19:48 -0300

    Fix pep8 issues in src/kimchi/objectstore.py
    Fix minor pep8 issues.
    
    Signed-off-by: Rodrigo Trujillo <rodrigo.trujillo@linux.vnet.ibm.com>

--- src/kimchi/objectstore.py
+++ src/kimchi/objectstore.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 import json
 import sqlite3
@@ -37,7 +37,6 @@ class ObjectStoreSession(object):
     def __init__(self, conn):
         self.conn = conn
         self.conn.text_factory = lambda x: unicode(x, "utf-8", "ignore")
-
 
     def get_list(self, obj_type):
         c = self.conn.cursor()
@@ -105,7 +104,8 @@ class ObjectStore(object):
         try:
             return self._connections[ident]
         except KeyError:
-            self._connections[ident] = sqlite3.connect(self.location, timeout=10)
+            self._connections[ident] = sqlite3.connect(self.location,
+                                                       timeout=10)
             if len(self._connections.keys()) > 10:
                 id, conn = self._connections.popitem(last=False)
                 conn.interrupt()
