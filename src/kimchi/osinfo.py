fe2355807d40933d28c0112787b96a3ce1f5c145
Paulo Vital <pvital@linux.vnet.ibm.com>
2014-06-10 12:38:31 -0300

    Add Ubuntu as modern distro to Power guests.
    Set Ubuntu 14.04 as base for modern Power guests.
    
    Signed-off-by: Paulo Vital <pvital@linux.vnet.ibm.com>

--- src/kimchi/osinfo.py
+++ src/kimchi/osinfo.py
@@ -59,7 +59,8 @@ template_specs = {'x86': {'old': dict(common_spec, disk_bus='ide',
 modern_version_bases = {'x86': {'debian': '6.0', 'ubuntu': '7.10',
                                 'opensuse': '10.3', 'centos': '5.3',
                                 'rhel': '6.0', 'fedora': '16', 'gentoo': '0'},
-                        'power': {'rhel': '7.0', 'fedora': '19'}}
+                        'power': {'rhel': '7.0', 'fedora': '19',
+                                  'ubuntu': '14.04'}}
 
 icon_available_distros = [icon[5:-4] for icon in glob.glob1('%s/images/'
                           % paths.ui_dir, 'icon-*.png')]
