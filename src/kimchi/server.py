kimchi-project/kimchi

commit e3b79e30c678bac0ab6150962932d2b36e0fe909

Storage volume upload: Control request body size of kimchi
This patch control request body size to be 4G,
when request size exceed, kimchi will response with 413.

Signed-off-by: Royce Lv <lvroyce@linux.vnet.ibm.com>
Signed-off-by: Crístian Viana <vianac@linux.vnet.ibm.com>

src/kimchi/server.py
@@ -87,6 +87,7 @@ class Server(object):
         # directly. You must go through the proxy.
         cherrypy.server.socket_host = '127.0.0.1'
         cherrypy.server.socket_port = options.cherrypy_port
+        cherrypy.server.max_request_body_size = eval(options.max_body_size)
 
         cherrypy.log.screen = True
         cherrypy.log.access_file = options.access_log

src/kimchid.in
@@ -86,6 +86,8 @@ def main(options):
     # Add non-option arguments
     setattr(options, 'ssl_cert', config.config.get('server', 'ssl_cert'))
     setattr(options, 'ssl_key', config.config.get('server', 'ssl_key'))
+    setattr(options, 'max_body_size',
+            config.config.get('server', 'max_body_size')*1024)
 
     kimchi.server.main(options)
 

src/kimchi.conf.in
@@ -30,6 +30,9 @@
 # at the same network
 #federation = off
 
+# Max request body size in KB, default value is 4GB
+#max_body_size = 4 * 1024 * 1024
+
 [logging]
 # Log directory
 #log_dir = @localstatedir@/log/kimchi

src/kimchi/config.py.in
@@ -255,6 +255,7 @@ def _get_config():
     config.set("server", "ssl_key", "")
     config.set("server", "environment", "production")
     config.set("server", "federation", "off")
+    config.set('server', 'max_body_size', '4*1024*1024')
     config.add_section("logging")
     config.set("logging", "log_dir", paths.log_dir)
     config.set("logging", "log_level", DEFAULT_LOG_LEVEL)

commit 57422c42ef1532d94da8313b70e2399ce2809601

Fix: Use "max_request_body_size" value as int instead of string
The CherryPy variable "max_request_body_size" should be a number value,
in bytes. The current code runs something like
"eval('max_body_size' * 1024)", where 'max_body_size' is the value read
from the external configuration file, in kilobytes. Multiplying a string
by a number in Python creates a new string repeating the original string
a number of times. We expect a multiplication, not a string repetition.
This bug was introduced by commit e3b79e3.

Multiply the number 1024 by another number (i.e. the "eval" result), not
a string.

Signed-off-by: Crístian Viana <vianac@linux.vnet.ibm.com>

src/kimchi/server.py
@@ -87,7 +87,9 @@ class Server(object):
         # directly. You must go through the proxy.
         cherrypy.server.socket_host = '127.0.0.1'
         cherrypy.server.socket_port = options.cherrypy_port
-        cherrypy.server.max_request_body_size = eval(options.max_body_size)
+
+        max_body_size_in_bytes = eval(options.max_body_size) * 1024
+        cherrypy.server.max_request_body_size = max_body_size_in_bytes
 
         cherrypy.log.screen = True
         cherrypy.log.access_file = options.access_log

src/kimchid.in
@@ -87,7 +87,7 @@ def main(options):
     setattr(options, 'ssl_cert', config.config.get('server', 'ssl_cert'))
     setattr(options, 'ssl_key', config.config.get('server', 'ssl_key'))
     setattr(options, 'max_body_size',
-            config.config.get('server', 'max_body_size')*1024)
+            config.config.get('server', 'max_body_size'))
 
     kimchi.server.main(options)
 