b61931d4b29bc1c3eb4c1087b6df70f7bc8d99e5
Aline Manera <alinefm@linux.vnet.ibm.com>
2015-04-23 12:17:20 -0300

    Verify storage pool set as Template default prior to server starts up
    The verification was being done in model/model.py but as it is only related to
    storage pools, it was added to model/storagepools.py
    
    This patch also adds a new option 'create_iso_pool' to kimchid, so the user can
    choose to create or not the ISO pool during server starts up.
    
    Signed-off-by: Aline Manera <alinefm@linux.vnet.ibm.com>

--- src/kimchi/model/model.py
+++ src/kimchi/model/model.py
@@ -1,7 +1,7 @@
 #
 # Project Kimchi
 #
-# Copyright IBM, Corp. 2014
+# Copyright IBM, Corp. 2014-2015
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -18,23 +18,12 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 import inspect
-import logging
 import os
-import sys
-
-import cherrypy
-import libvirt
-import lxml.etree as ET
-from lxml.builder import E
 
 from kimchi.basemodel import BaseModel
 from kimchi.model.libvirtconnection import LibvirtConnection
 from kimchi.objectstore import ObjectStore
 from kimchi.utils import import_module, listPathModules
-
-
-DEFAULT_POOLS = {'default': {'path': '/var/lib/libvirt/images'},
-                 'ISO': {'path': '/var/lib/kimchi/isos'}}
 
 
 class Model(BaseModel):
@@ -43,10 +32,6 @@ class Model(BaseModel):
         self.objstore = ObjectStore(objstore_loc)
         self.conn = LibvirtConnection(libvirt_uri)
         kargs = {'objstore': self.objstore, 'conn': self.conn}
-
-        if self.conn.isQemuURI():
-            for pool_name, pool_arg in DEFAULT_POOLS.iteritems():
-                self._default_pool_check(pool_name, pool_arg)
 
         this = os.path.basename(__file__)
         this_mod = os.path.splitext(this)[0]
@@ -64,30 +49,3 @@ class Model(BaseModel):
                         models.append(instance(**kargs))
 
         return super(Model, self).__init__(models)
-
-    def _default_pool_check(self, pool_name, pool_arg):
-        conn = self.conn.get()
-        pool = E.pool(E.name(pool_name), type='dir')
-        pool.append(E.target(E.path(pool_arg['path'])))
-        xml = ET.tostring(pool)
-        try:
-            pool = conn.storagePoolLookupByName(pool_name)
-        except libvirt.libvirtError:
-            try:
-                pool = conn.storagePoolDefineXML(xml, 0)
-                # Add build step to make sure target directory created
-                pool.build(libvirt.VIR_STORAGE_POOL_BUILD_NEW)
-                pool.setAutostart(1)
-            except libvirt.libvirtError, e:
-                cherrypy.log.error("Fatal: Cannot create default pool because "
-                                   "of %s, exit kimchid" % e.message,
-                                   severity=logging.ERROR)
-                sys.exit(1)
-
-        if pool.isActive() == 0:
-            try:
-                pool.create(0)
-            except libvirt.libvirtError, e:
-                err = "Fatal: Default pool cannot be activated, exit kimchid"
-                cherrypy.log.error(err, severity=logging.ERROR)
-                sys.exit(1)
