b6d2242925813d40df975d59b56def661c2563a1
Paulo Vital <pvital@linux.vnet.ibm.com>
2015-10-02 08:28:22 -0300

    Update the header of all Wok back-end files
    Signed-off-by: Paulo Vital <pvital@linux.vnet.ibm.com>

--- src/wok/proxy.py
+++ src/wok/proxy.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 #
-# Project Kimchi
+# Project Wok
 #
 # Copyright IBM, Corp. 2014-2015
+#
+# Code derived from Project Kimchi
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
