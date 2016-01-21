bd28f1446f5b0aecb07330b96710c1322d3f815d
Samuel Guimarães <sguimaraes943@gmail.com>
2016-01-21 14:58:59 -0500

    Implementing sort for Templates tab
    Signed-off-by: Andre Teodoro <andreteodoro.work@gmail.com>

--- ui/js/src/kimchi.template_main.js
+++ ui/js/src/kimchi.template_main.js
@@ -1,7 +1,7 @@
 /*
  * Project Kimchi
  *
- * Copyright IBM, Corp. 2013-2014
+ * Copyright IBM, Corp. 2013-2016
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,16 +90,16 @@ kimchi.templateBindClick = function() {
     });
 
     $('#gallery-table-button').on('click', function(event) {
-        if ($("#templates-grid").hasClass('wok-vm-list')) {
-            $("#templates-grid").removeClass("wok-vm-list");
-            $("#templates-grid").addClass("wok-vm-gallery");
-            $("#gallery-table-button").html("View Table <i class='fa fa-angle-right'></i><i class='fa fa-angle-right'></i><i class='fa fa-angle-right'></i>");
-        } else {
-            $("#templates-grid").removeClass("wok-vm-gallery");
-            $("#templates-grid").addClass("wok-vm-list");
-            $("#gallery-table-button").html("View Gallery <i class='fa fa-angle-right'></i><i class='fa fa-angle-right'></i><i class='fa fa-angle-right'></i>");
-        }
+        $(".wok-vm-list, .wok-vm-gallery").toggleClass("wok-vm-list wok-vm-gallery");
+        var text = $('#gallery-table-button span.text').text();
+        $('#gallery-table-button span.text').text(text == i18n['KCHTMPL6005M'] ? i18n['KCHTMPL6004M'] : i18n['KCHTMPL6005M']);
     });
+
+    $('.sort').on('click', function(event) {
+        event.preventDefault();
+        $('.filter-option').text($(this).text());
+    });
+
 }
 kimchi.hideTitle = function() {
     $('#tempTitle').hide();
