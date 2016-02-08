0b51e56801637e1d473dd574f511b9546e4f08a1
Socorro Stoppler <socorro@linux.vnet.ibm.com>
2016-02-08 08:44:59 -0800

    Create new volume with .img extension and attach to VM
    Default to 'qcow2' and not list kimchi_isos for new disk
    
    Signed-off-by: Socorro Stoppler <socorro@linux.vnet.ibm.com>

--- ui/js/src/kimchi.guest_storage_add.main.js
+++ ui/js/src/kimchi.guest_storage_add.main.js
@@ -15,6 +15,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+kimchi.switchPage = function(fromPageId, toPageId, direction) {
+    $('.tab-content').css('overflow', 'hidden');
+    direction = direction || 'left';
+    var toLeftBegin;
+    var fromLeftEnd;
+    if ('left' === direction) {
+        toLeftBegin = '100%';
+        fromLeftEnd = '-100%';
+    } else if ('right' === direction) {
+        toLeftBegin = '-100%';
+        fromLeftEnd = '100%';
+    }
+    var formPage = $('#' + fromPageId);
+    var toPage = $('#' + toPageId);
+    toPage.css({
+        left: toLeftBegin
+    });
+    formPage.animate({
+        left: fromLeftEnd,
+        opacity: 0.1
+    }, 400, function() {
+        $('.tab-content').css('overflow', 'visible');
+    });
+    toPage.animate({
+        left: '0',
+        opacity: 1
+    }, 400, function() {
+        $('.tab-content').css('overflow', 'visible');
+    });
+};
+
 kimchi.guest_storage_add_main = function() {
     var types = [{
         label: 'cdrom',
@@ -35,9 +66,27 @@ kimchi.guest_storage_add_main = function() {
     var pathTextbox = $('input[name="path"]', storageAddForm);
     var poolTextbox = $('select#guest-disk-pool', storageAddForm);
     var volTextbox = $('select#guest-disk-vol', storageAddForm);
+    var newPoolTextbox = $('select#guest-disk-pool-new', storageAddForm);
+    var capacityTextbox = $('input[name="capacity"]', storageAddForm);
+    var formatTextbox = $('select#guest-disk-format-new', storageAddForm);
     var selectStorageTypeHTML = '';
     var selectStoragePoolHTML = '';
     var selectStorageVolHTML  = '';
+    var rbExisting = 'false';
+
+    var getFormatList = function() {
+        var format = ["bochs", "cloop", "cow", "dmg", "qcow", "qcow2", "qed", "raw", "vmdk", "vpc"];
+        var selectFormatHTML = '';
+        var i;
+        for (i = 0; i < format.length; i++) {
+            selectFormatHTML += '<option value="'+ format[i] + '">' + format[i] + '</option>';
+        }
+        formatTextbox.empty();
+        formatTextbox.append(selectFormatHTML);
+        $(formatTextbox).change();
+        formatTextbox.selectpicker();
+        $('.selectpicker').selectpicker('refresh');
+    };
 
     typeTextbox.change(function() {
         var pathObject = {'cdrom': ".path-section", 'disk': '.volume-section'};
@@ -45,41 +94,87 @@ kimchi.guest_storage_add_main = function() {
         $.each(pathObject, function(type, value) {
             if(selectType === type){
                 $(value).removeClass('hidden');
+            } else if ((selectType === null) && (type === 'disk')) {
+                $(value).removeClass('hidden');
             } else {
                 $(value).addClass('hidden');
             }
         });
-
         if ($(".path-section").hasClass('hidden')) {
-            $(poolTextbox).val('default');
-            $(poolTextbox).change();
             $(pathTextbox).val("");
-        }
-        else {
+            if ($('#new-disk').checked) {
+                $('#existing-disk-box').addClass('hidden');
+                $(newPoolTextbox).val('default');
+                $(newPoolTextbox).change();
+                $(formatTextbox).val('qcow2');
+                $(formatTextbox).change();
+            } else if ($('#existing-disk').checked) {
+                $('#new-disk-box').addClass('hidden');
+                $(poolTextbox).val('default');
+                $(poolTextbox).change();
+            } else {
+                //Goes here the first time since radiobuttons are undefined
+                if (rbExisting === 'true') {
+                    $('#new-disk-box').addClass('hidden');
+                } else {
+                    $('#existing-disk-box').addClass('hidden');
+                    $(formatTextbox).val('qcow2');
+                    $(formatTextbox).change();
+                }
+            }
+        } else {
             $(poolTextbox).val("");
             $(volTextbox).val("");
+            $(newPoolTextbox).val("");
+            $(capacityTextbox).val("");
+            $(formatTextbox).val("");
         }
         $('.selectpicker').selectpicker('refresh');
     });
 
-    kimchi.listStoragePools(function(result) {
-        var options = [];
-        if (result && result.length) {
-            $.each(result, function(index, storagePool) {
-                if ((storagePool.state==="active") && (storagePool.type !== 'kimchi-iso')) {
-                    options.push({
-                        label: storagePool.name,
-                        value: storagePool.name
-                        });
-                    selectStoragePoolHTML += '<option value="'+ storagePool.name + '">' + storagePool.name + '</option>';
+    var getStoragePools = function(radioButton) {
+        kimchi.listStoragePools(function(result) {
+            var options = [];
+            selectStoragePoolHTML = ''; //reset string
+            if (result && result.length) {
+                $.each(result, function(index, storagePool) {
+                    if (radioButton === 'existing') {
+                        if ((storagePool.state==="active") && (storagePool.type !== 'kimchi-iso')) {
+                            options.push({
+                                label: storagePool.name,
+                                value: storagePool.name
+                                });
+                            selectStoragePoolHTML += '<option value="'+ storagePool.name + '">' + storagePool.name + '</option>';
+                        }
+                    } else { //new disk
+                        if ((storagePool.type !== 'iscsi') && (storagePool.type !== 'scsi') && (storagePool.type !== 'kimchi-iso')) {
+                            options.push({
+                                label: storagePool.name,
+                                value: storagePool.name
+                                });
+                            selectStoragePoolHTML += '<option value="'+ storagePool.name + '">' + storagePool.name + '</option>';
+                        }
                     }
-
                 });
-            poolTextbox.append(selectStoragePoolHTML);
-            poolTextbox.val(options[0].value);
-            poolTextbox.selectpicker();
-        }
-    });
+                if (radioButton === 'existing') {
+                    poolTextbox.empty();
+                    poolTextbox.append(selectStoragePoolHTML);
+                    $(poolTextbox).change();
+	            poolTextbox.selectpicker();
+                    $('.selectpicker').selectpicker('refresh');
+                } else if (radioButton === 'new') { //new disk
+                    newPoolTextbox.empty();
+                    newPoolTextbox.append(selectStoragePoolHTML);
+                    $(newPoolTextbox).val("qcow2");
+                    newPoolTextbox.selectpicker();
+                    getFormatList();
+                }
+            }
+        });
+    };
+
+    //First time retrieving list of Storage Pools - defaulting to new disk
+    getStoragePools('new');
 
     poolTextbox.change(function() {
         var options = [];
@@ -109,12 +204,14 @@ kimchi.guest_storage_add_main = function() {
                     $(volTextbox).prop('disabled',true);
                     $(submitButton).prop('disabled', true);
                 }
-                volTextbox.selectpicker();
-                $('.selectpicker').selectpicker('refresh');
+            } else {
+                $(volTextbox).prop('disabled',true);
+                $(submitButton).prop('disabled', true);
             }
+            volTextbox.selectpicker();
+            $('.selectpicker').selectpicker('refresh');
         }, null, false);
     });
-
 
     typeTextbox.change(function() {
         var pathObject = {'cdrom': ".path-section", 'disk': '.volume-section'};
@@ -126,6 +223,49 @@ kimchi.guest_storage_add_main = function() {
                 $(value).addClass('hidden');
             }
         });
+    });
+
+    var currentPage = 'new-disk-box';
+    $('#existing-disk').change(function() {
+        if (this.checked) {
+            rbExisting = 'true';
+            if (currentPage === 'new-disk-box') {
+                kimchi.switchPage(currentPage, 'existing-disk-box', 'right');
+            }
+            currentPage = 'existing-disk-box';
+            $('#existing-disk-box').removeClass('hidden');
+            $('#new-disk-box').addClass('hidden');
+            $('#guest-storage-add-window .modal-body .template-pager').animate({
+                height: "200px"
+            }, 300);
+            getStoragePools('existing');
+            $(pathTextbox).val("");
+            $(newPoolTextbox).val("");
+            $(capacityTextbox).val("");
+            $(formatTextbox).val("");
+        }
+    });
+
+    $('#new-disk').change(function() {
+        if (this.checked) {
+            $(formatTextbox).val("qcow2");
+            $(formatTextbox).change();
+            rbExisting = 'false';
+            if (currentPage === 'existing-disk-box') {
+                kimchi.switchPage(currentPage, 'new-disk-box', 'right');
+            } else if($(capacityTextbox).is(":visible") === false ) {
+                 kimchi.switchPage(currentPage, 'new-disk-box', 'right');
+            }
+            currentPage = 'new-disk-box';
+            $('#existing-disk-box').addClass('hidden');
+            $('#new-disk-box').removeClass('hidden');
+            $('#guest-storage-add-window .modal-body .template-pager').animate({
+                height: "300px"
+            }, 400);
+            $(pathTextbox).val("");
+            $(poolTextbox).val("");
+            $(volTextbox).val("");
+        }
     });
 
     if (kimchi.thisVMState === 'running') {
@@ -155,15 +295,104 @@ kimchi.guest_storage_add_main = function() {
         }
     };
 
-    var validateDisk = function(settings) {
-        if (settings['pool'] && settings['vol']){
-           // Delete path property since it's not needed for disk
-           delete settings['path'];
-           return true;
+    var onError = function(result) {
+        if(!result) {
+            return;
         }
-        else {
-            wok.message.error(i18n['KCHVMSTOR0002E'],'#alert-modal-container2');
-            return false;
+        var msg = result['message'] || (
+            result['responseJSON'] && result['responseJSON']['reason']
+        );
+        wok.message.error(msg);
+    };
+
+    var addStorage = function(settings) {
+        kimchi.addVMStorage(settings, function(result) {
+            wok.window.close();
+            wok.topic('kimchi/vmCDROMAttached').publish({
+            result: result
+            });
+        }, function(result) {
+            var errText = result['reason'] ||
+            result['responseJSON']['reason'];
+            wok.message.error(errText, '#alert-modal-container2');
+            $.each([submitButton, pathTextbox, poolTextbox, volTextbox, newPoolTextbox, capacityTextbox, formatTextbox], function(i, c) {
+                $(c).prop('disabled', false);
+            });
+        });
+    }
+
+    var createVol = function(settings, addVolSettings) {
+        kimchi.createVolumeWithCapacity('default', {
+            name: settings['vol'],
+            format: settings['format'],
+            capacity: settings['capacity']
+        }, function(result) {
+            var taskId = result.id;
+            function monitorTask() {
+                kimchi.getTask(taskId, function(result) {
+                    var status = result.status;
+                    if (status === "finished") {
+                        //Now add newly created volume to VM
+                        addStorage(addVolSettings);
+                    } else if (status === "running") {
+                        setTimeout(monitorTask, 2000);
+                        $(submitButton).prop('disabled', true);
+                    } else if (status === "failed") {
+                        var errText = result['reason'] ||
+                        result['responseJSON']['reason'];
+                        $(submitButton).prop('disabled', true);
+                        wok.message.error(errText, '#alert-modal-container2');
+                    }
+                });
+            }
+            setTimeout(monitorTask, 2000);
+        }, onError);
+    };
+
+    var bNewDisk = 'false';
+
+    var validateDisk = function(settings) {
+        // Determine whether it's existing disk or new disk
+        if($(capacityTextbox).is(":visible") === true ) {
+            bNewDisk = 'true';
+        }
+        if (bNewDisk === 'true') {
+            if (settings['newpool'] && settings['capacity'] && settings['format']){
+                //Change settings['newpool'] to settings['pool']
+                settings['pool']=settings['newpool'];
+                var vmname = settings['vm'];
+                vmname = vmname + new Date().getTime();
+                //Unique vol name to be created
+                settings['vol']=vmname + ".img";
+                //This is all that is needed for attaching newly created volume to VM
+                var addVolSettings = {
+                    vm: settings['vm'],
+                    type: settings['type'],
+                    vol:  settings['vol'],
+                    pool: settings['pool']
+                };
+                var sizeInMB = parseInt(settings['capacity']) * 1024;
+                settings['capacity'] = sizeInMB;
+                //These need to be deleted so they don't get passed to backend
+                delete settings['path'];
+                delete settings['newpool'];
+                //Create an empty storage volume and attach to VM if successful
+                createVol(settings, addVolSettings);
+                return true;
+            } else {
+                wok.message.error(i18n['KCHVMSTOR0002E'],'#alert-modal-container2');
+                return false;
+            }
+        } else {
+            if (settings['pool'] && settings['vol']){
+                // Delete path property since it's not needed for disk
+                delete settings['path'];
+                return true;
+            }
+            else {
+                wok.message.error(i18n['KCHVMSTOR0002E'],'#alert-modal-container2');
+                return false;
+            }
         }
     };
 
@@ -172,6 +401,11 @@ kimchi.guest_storage_add_main = function() {
         if (submitButton.prop('disabled')) {
             return false;
         }
+        var bNewDisk = 'false';
+        // Determine whether it's existing disk or new disk
+        if($(capacityTextbox).is(":visible") === true ) {
+            bNewDisk = 'true';
+        }
 
         var formData = storageAddForm.serializeObject();
         var settings = {
@@ -179,40 +413,30 @@ kimchi.guest_storage_add_main = function() {
             type: typeTextbox.val(),
             path: pathTextbox.val(),
             pool: poolTextbox.val(),
-            vol: volTextbox.val()
+            vol: volTextbox.val(),
+            newpool: newPoolTextbox.val(),
+            format: formatTextbox.val(),
+            capacity: capacityTextbox.val()
         };
 
         $(submitButton).prop('disabled', true);
-        $.each([pathTextbox, poolTextbox, volTextbox], function(i, c) {
+        $.each([pathTextbox, poolTextbox, volTextbox, newPoolTextbox, capacityTextbox, formatTextbox], function(i, c) {
             $(c).prop('disabled', true);
         });
         // Validate form for cdrom and disk
         validateSpecifiedForm = validator[settings['type']];
         if (!validateSpecifiedForm(settings)) {
             $(submitButton).prop('disabled', false);
-            $.each([submitButton, pathTextbox, poolTextbox, volTextbox], function(i, c) {
+            $.each([submitButton, pathTextbox, poolTextbox, volTextbox, newPoolTextbox, capacityTextbox, formatTextbox], function(i, c) {
                 $(c).prop('disabled', false);
             });
             return false;
         }
         $(submitButton).addClass('loading').text(i18n['KCHVMCD6003M']);
 
-        kimchi.addVMStorage(settings, function(result) {
-            wok.window.close();
-            wok.topic('kimchi/vmCDROMAttached').publish({
-                result: result
-            });
-        }, function(result) {
-            var errText = result['reason'] ||
-                result['responseJSON']['reason'];
-            wok.message.error(errText, '#alert-modal-container2');
-
-            $.each([submitButton, pathTextbox, poolTextbox, volTextbox], function(i, c) {
-                $(c).prop('disabled', false);
-            });
-            $(submitButton).removeClass('loading').text(i18n['KCHVMCD6002M']);
-        });
-
+        if(bNewDisk === 'false'){
+            addStorage(settings);
+        }
         event.preventDefault();
     };
 
@@ -222,6 +446,9 @@ kimchi.guest_storage_add_main = function() {
         $(submitButton).prop('disabled', $(this).val() === '');
     });
     volTextbox.on('change propertychange', function (event) {
+        $(submitButton).prop('disabled', $(this).val() === '');
+    });
+    capacityTextbox.on('change input propertychange', function(event) {
         $(submitButton).prop('disabled', $(this).val() === '');
     });
 
