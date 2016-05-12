4f5806211f80090e204829bbd906739db1b2d9e4
Paulo Vital <pvital@linux.vnet.ibm.com>
2016-05-12 18:32:26 -0300

    Make Cheerypy up if not able to connect to libvirt
    Modified libvirtconnection.py to not stop Cherrypy server when not able to
    connect to libvirt. Now, a notification message is displayed in UI to warn user
    about libvirtd missing connection, making the web server available to provide
    the other plugins from wok.
    
    This patch is part of the solution for Issue #201
    
    Signed-off-by: Paulo Vital <pvital@linux.vnet.ibm.com>

--- i18n.py
+++ i18n.py
@@ -331,4 +331,6 @@ messages = {
 
     "KCHLVMS0001E": _("Invalid volume group name parameter: %(name)s."),
 
+    "KCHCONN0001E": _("Unable to establish connection with libvirt. Please check your libvirt URI which is often defined in /etc/libvirt/libvirt.conf"),
+
 }
