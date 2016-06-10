03d1a2b9a700a296f0ab6f195424d3b7f700ddd2
Ramon Medeiros <ramonn@linux.vnet.ibm.com>
2016-06-10 12:47:37 -0300

    Improve UI error codes checking
    Signed-off-by: Ramon Medeiros <ramonn@linux.vnet.ibm.com>

--- check_ui_code_errors.sh
+++ check_ui_code_errors.sh
@@ -20,7 +20,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 errors="$(cat ui/pages/i18n.json.tmpl | grep -o 'KCH[0-9A-Z]*'| sort)"
-uiErrors="$(grep -Ro 'KCH[0-9A-Z]*' ui/js/ | cut -d: -f2 | sort| uniq)"
+uiErrors="$(grep -ERo 'KCH[0-9A-Z]+' ui/js/ | cut -d: -f2 | sort| uniq)"
 
 # all errors on i18n are present in js/html files: success
 if [ "$errors" == "$uiErrors" ]; then
