14c73318136cf125948ef9471104048d50efde1d
Jose Ricardo Ziviani <joserz@linux.vnet.ibm.com>
2016-08-15 09:11:21 -0300

    Fix frontend vcpu hotplug
     - this commit updates the max vcpus value in order to fix the hotplug.
    
    Signed-off-by: Jose Ricardo Ziviani <joserz@linux.vnet.ibm.com>

--- ui/js/src/kimchi.guest_edit_main.js
+++ ui/js/src/kimchi.guest_edit_main.js
@@ -813,12 +813,13 @@ kimchi.guest_edit_main = function() {
             for (var key in data) {
                 valueFromUI = data[key];
                 if (valueFromUI instanceof Object) {
-                    // Compare if Objects of original and data are identical
-                    // Handle special case when key is memory and guest is running as valueFromUI will return a null for max mem
-                    // since it is disabled; for cpu_info, when guest is running, just skip it since no processing is required
+                    // Compare if Objects of original and data are identical.
+                    // Handle special case when key is memory and guest is
+                    // running as valueFromUI will return a null for max mem
+                    // since it is disabled.
                     if (kimchi.thisVMState === 'running' || kimchi.thisVMState === 'paused') {
                         if (key === 'cpu_info') {
-                            continue;
+                            data['cpu_info']['maxvcpus'] = org.cpu_info.maxvcpus;
                         }
                         if (key === 'memory') {
                             // Replace valueFromUI of max mem with one from original as otherwise the value is undefined
