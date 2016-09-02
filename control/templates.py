b09bbafd4cd69c24788f7432a8bd6f264405111f
Archana Singh <archus@linux.vnet.ibm.com>
2016-09-02 09:23:31 +0530

    Updated code to support 'interfaces' parameter to template API only on s390x/s390 architecture.
    1) Added i18n error codes.
    2) Added interface as new kimchitype in API.json. And ref added in update template.
         -Interface expects an object with parameters: 'name', 'type' and 'mode'.
         -Name should be name of host network interface(Ethernet, Bond, VLAN) for type 'macvtap'
          or name should be name of host openvswitch bridge interface for type ovs.
         -Mode only applicable for interface type macvtap to indicates whether packets
          has to be delivered directly to target device(bridge) or to the external bridge(vepa-capable bridge).
         -Mode is Optional.
    3) Added interfaces as optional attribute in template control.
         -This attribute returns to be list of host interface attached to template,
          each interface will have above attribute.
         -Empty list if no host network interface is attached to template.
    4) Added method to generate valid interface xml from the interface based on above attribute.
         -This will use below method to generate interface xml based on type and other attribute.
         -Update vm xml generate method to add generated interfaces xml to vm xml.
    5) Modified xmlutils which generates interface xml based on interface type and attributes.
    6) Added validate_interfaces method to validate that interfaces are only
    supported on s390x/s390 architecture.
    7) network in default template to empty on s390x/s390 architecture.
    
    Signed-off-by: Archana Singh <archus@linux.vnet.ibm.com>

--- control/templates.py
+++ control/templates.py
@@ -17,6 +17,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
+import os
+
 from wok.control.base import Collection, Resource
 from wok.control.utils import UrlSubNode
 
@@ -58,7 +60,7 @@ class Template(Resource):
 
     @property
     def data(self):
-        return {
+        info = {
             'name': self.ident,
             'icon': self.info['icon'],
             'invalid': self.info['invalid'],
@@ -72,3 +74,7 @@ class Template(Resource):
             'graphics': self.info['graphics'],
             'cpu_info': self.info.get('cpu_info')
         }
+        if os.uname()[4] in ['s390x', 's390']:
+            info['interfaces'] = self.info.get('interfaces', [])
+
+        return info
