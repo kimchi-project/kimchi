aa0693812b0958e63661b380eb632ca17789456f
Ramon Medeiros <ramonn@linux.vnet.ibm.com>
2017-01-19 16:06:21 -0200

    Bug fix #1029: Unable to create a snapshot on a running guest
    Libvirt supports live snapshot. This patch just removes the limitation
    to run snapshot only on turned off guests
    
    Signed-off-by: Ramon Medeiros <ramonn@linux.vnet.ibm.com>

--- i18n.py
+++ i18n.py
@@ -344,7 +344,6 @@ messages = {
     "KCHVMSTOR0020E": _("On s390x arch 'format' must be specified while attaching disk to virtual machine"),
     "KCHVMSTOR0021E": _("Virtual disk already exists on the system: %(disk_path)s"),
 
-    "KCHSNAP0001E": _("Virtual machine '%(vm)s' must be stopped before creating a snapshot of it."),
     "KCHSNAP0002E": _("Unable to create snapshot '%(name)s' on virtual machine '%(vm)s'. Details: %(err)s"),
     "KCHSNAP0003E": _("Snapshot '%(name)s' does not exist on virtual machine '%(vm)s'."),
     "KCHSNAP0004E": _("Unable to retrieve snapshot '%(name)s' on virtual machine '%(vm)s'. Details: %(err)s"),
