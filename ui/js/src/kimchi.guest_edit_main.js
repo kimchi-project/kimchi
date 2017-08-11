4d1eb784ac35d05e3981f127e436313797c8745d
Fred <fpalluel@gmail.com>
2017-08-11 22:12:41 +0200

    Extract autostart checkbox state

--- ui/js/src/kimchi.guest_edit_main.js
+++ ui/js/src/kimchi.guest_edit_main.js
@@ -1156,6 +1156,7 @@ kimchi.guest_edit_main = function() {
         kimchi.retrieveVM(kimchi.selectedGuest, function(org) {
             $(saveButton).prop('disabled', true);
             var data = $('#form-guest-edit-general').serializeObject();
+            data['autostart'] = $("#enableGuestAutostart").prop('checked');
             data['memory'] = {current: Number(data['memory-ui']), maxmemory: Number(data['max-memory'])};
 
             var changedFields = {};
