cc4c9095b1d39715ceb84eef6db4fa3bc5c7b6ac
Aline Manera <aline.manera@gmail.com>
2019-05-23 13:05:07 -0400

    Use importlib module instead of imp
    imp module was deprecated in python3
    
    Signed-off-by: Aline Manera <aline.manera@gmail.com>

--- contrib/check_i18n.py
+++ contrib/check_i18n.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
-import imp
+import importlib
 import os
 import re
 import sys
@@ -37,10 +37,8 @@ BAD_PATTERN = re.compile(r'%\([^)]*?\)')
 
 
 def load_i18n_module(i18nfile):
-    path = os.path.dirname(i18nfile)
-    mname = i18nfile.replace('./', '_').replace('/', '_').rstrip('.py')
-    mobj = imp.find_module('i18n', [path])
-    return imp.load_module(mname, *mobj)
+    mname = i18nfile.replace('/', '.').rstrip('.py').lstrip('src.')
+    return importlib.import_module(mname)
 
 
 def check_string_formatting(messages):
